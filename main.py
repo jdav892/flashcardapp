7733c2e71ba6bf7a6fafe6dd8ee5e135f078d3e0
jdav892 <jadavila9@gmail.com>
2024-07-02 01:03:43 -0400

    ui

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas
 
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50, bg="lightblue")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 
 x_image = PhotoImage(file="C:/Users/jay-5/Documents/code/pythonProj/100daysOfPy/flashCards/images/wrong.png")
@@ -18,9 +18,12 @@ check_image = PhotoImage(file="C:/Users/jay-5/Documents/code/pythonProj/100daysO
 check_button = Button(image=check_image, highlightthickness=0)
 check_button.grid(column=1, row=1)
 
-canvas = Canvas(width=900, height=600)
+canvas = Canvas(width=800, height=526)
 front_image = PhotoImage(file="C:/Users/jay-5/Documents/code/pythonProj/100daysOfPy/flashCards/images/card_front.png")
-canvas.create_image(400, 526, image=front_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_image(400, 263, image=front_image)
 canvas.grid(column=0, row=0, columnspan=2)
+canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Arial", 40, "bold"))
 
 window.mainloop()
